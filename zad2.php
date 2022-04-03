cecbf6f4f676b0d5a1854f3fd530461cf8608724
ppavlovi1 <102903091+ppavlovi1@users.noreply.github.com>
2022-04-03 13:22:32 +0200

    Add files via upload

---
+++ zad2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+$a="50";
+$b="Smith";
+$c="Jill";
+echo "
+<table>
+<tr>
+  <th>Age</th>
+  <th>Lastname</th>
+  <th>Firstname</th>
+</tr>
+<tr>
+  <td>$a</td>
+  <td>$b</td>
+  <td>$c</td>
+</tr>
+
+</table>"
+?>
+
+</body>
+</html>
