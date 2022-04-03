1ebb6f8b8f376f17a55cdc919b7654fff393acfd
ppavlovi1 <102903091+ppavlovi1@users.noreply.github.com>
2022-04-03 13:05:56 +0200

    Add files via upload

---
+++ zad1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+$a = "1848";
+$b = "1859";
+$c = "19";
+echo "Josip grof Jelačić Bužimski, bio je general i ban hrvatski, dalmatinski i slavonski od $a. do $b. godine, član plemićke obitelji Jelačić,
+ te jedan od najznačajnijih hrvatskih političara, kako u $c. stoljeću, tako i u cijeloj povijesti Hrvata. Ukinuo je kmetstvo i sazvao prve izbore za Hrvatski sabor.
+  Kao vojskovođa je pobijedio u nizu bitaka protiv ustanika u 
+  Habsburškoj Monarhiji za vrijeme Revolucije u Mađarskoj $a. godine. U Hrvatskoj se slavi kao nacionalni 
+  junak.";
+?>
+
+</body>
+</html>
